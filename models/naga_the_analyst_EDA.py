970e97e9e1189ea3fb63f290d52c5b9e17580d5d
Mark Louise Badua <71631480+Mark-Badua@users.noreply.github.com>
2021-06-10 13:19:38 +0800

    Version 1

---
+++ models/naga_the_analyst_EDA.py
@@ -0,0 +1,20 @@
+def univariate_visualization(df):
+    sns.set(style="ticks")
+    for col in df:
+        if is_numeric_dtype(df[col]):
+            f, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.10, .90)}, figsize = (12, 12))
+            sns.boxplot(df[col], ax=ax_box)
+            sns.distplot(df[col], ax=ax_hist, kde =False)
+            ax_box.set(yticks=[])
+            sns.despine(ax=ax_hist)
+            sns.despine(ax=ax_box, left=True)
+            ax_hist.set_ylabel('Frequency')
+            ax_box.set_xlabel(None)
+        elif is_string_dtype(df[col]):
+            if len(df[col].value_counts()) > 1:
+                max_char_len = 0
+
+                plt.figure(figsize = (len(df[col].unique()) * 0.75, 7))
+                df[col].value_counts().plot(kind = 'bar', fontsize = 25)
+                plt.ylabel('Frequency', fontsize = 25)          
+    plt.show()
